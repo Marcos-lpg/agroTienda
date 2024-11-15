4ac1d29401a115b846facb59266ae1fd33e35a1b
Marcos-lpg <chazmarcos6@gmail.com>
2024-11-15 09:30:10 -0600

    crea clase usuario prueba dbcontext

---
+++ obj/models/products.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace agroTienda.obj.models
+{
+    public class products
+    {
+        
+    }
+}
